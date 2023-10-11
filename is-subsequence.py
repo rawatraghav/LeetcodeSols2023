08c2b9cb42edee1dac50456e2744451db647440f
Raghav Rawat <55054079+rawatraghav@users.noreply.github.com>
2023-10-11 11:09:48 -0400

    file updates

--- is-subsequence.py
+++ is-subsequence.py
@@ -1,4 +1,9 @@
+# https://leetcode.com/problems/is-subsequence/description/
+
 # https://leetcode.com/problems/is-subsequence/submissions/
+
+# Best Solution
+# https://leetcode.com/problems/is-subsequence/solutions/4074367/93-76-two-pointers-dp/
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
