90125c1c79ba8e248da76456db98cea15f067e2c
Raghav Rawat <55054079+rawatraghav@users.noreply.github.com>
2024-01-29 17:43:00 +0000

    dfs

---
+++ Binary Tree DFS/number-of-islands.py
@@ -0,0 +1,32 @@
+
+# https://leetcode.com/problems/number-of-islands/description/
+
+# DFS
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        if not grid:
+            return 0
+        
+        count= 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] == "1":
+                    count += 1
+                    self.search_island(grid, i, j)
+            
+        return count
+        
+    def search_island(self, grid, i, j):
+        
+        if i<0 or j<0 or i>=len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+            return 
+        grid[i][j] = "#"
+        self.search_island(grid, i-1, j)
+        self.search_island(grid, i+1, j)
+        self.search_island(grid, i, j+1)
+        self.search_island(grid, i, j-1)
+        
